28f8127e79da5c14e2b2a8cf494b47ce70577d41
Arquma <aaronquma@gmail.com>
2015-04-20 11:34:47 -0600

    generacion de las entidades de nuevo

--- proyectoUCROdontologia/src/ConnectionBDBundle/Entity/Expediente.php
+++ proyectoUCROdontologia/src/ConnectionBDBundle/Entity/Expediente.php
@@ -14,6 +14,11 @@ class Expediente
      */
     private $idexpediente;
 
+    /**
+     * @var \ConnectionBDBundle\Entity\Paciente
+     */
+    private $idpaciente;
+
 
     /**
      * Get idexpediente
@@ -23,5 +28,28 @@ class Expediente
     public function getIdexpediente()
     {
         return $this->idexpediente;
+    }
+
+    /**
+     * Set idpaciente
+     *
+     * @param \ConnectionBDBundle\Entity\Paciente $idpaciente
+     * @return Expediente
+     */
+    public function setIdpaciente(\ConnectionBDBundle\Entity\Paciente $idpaciente = null)
+    {
+        $this->idpaciente = $idpaciente;
+    
+        return $this;
+    }
+
+    /**
+     * Get idpaciente
+     *
+     * @return \ConnectionBDBundle\Entity\Paciente 
+     */
+    public function getIdpaciente()
+    {
+        return $this->idpaciente;
     }
 }
